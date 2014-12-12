bcf7600b13b4fcf66ca95a46270ac4a87b5bf28d
Sean Grimes <spg63@cs.drexel.edu>
2014-12-12 14:30:51 -0500

    added

---
+++ d/printOdds/printOdds.d
@@ -0,0 +1,67 @@
+﻿module main;
+
+import std.stdio;
+
+void printOdds(int x);
+void main(string[] args){
+	int x = 1;
+	printOdds(x);
+}
+
+void printOdds(int x){
+	if(x == 99){
+		// Just showing that write only prints the required output
+		// Does not end line and move onto next
+		// Can call writeln() without args to move onto a new line
+		// 	for more output
+		write(x);
+		writeln();
+	}
+	if ((x % 2 != 0) && (x < 99)){
+		write(x);
+		writeln();
+		printOdds(x + 2);
+	}
+	if((x % 2 == 0) && (x < 99)){
+		printOdds(x + 1);
+	}
+}
+
+
+/**
+ * Original C++ code
+ * Not very different for something simple
+ * Same logic, function declarations, the main difference
+ * 	is in the input/output.
+ * I'll move onto something slightly more involved next. 
+ * 
+ * #include <iostream>                                                             
+ * using std::cout;                                                                
+ * 
+ * void printOdds(int x);                                                          
+ *
+ * int main() {                                                                    
+ *     int x = 1;                                                                  
+ *     printOdds(x);                                                               
+ *
+ *     return 0;                                                                   
+ * }                                                                               
+ *
+ * void printOdds(int x){                                                          
+ *     // End the recursion                                                        
+ *     if(x == 99){                                                                
+ *         cout << x << "\n";                                                      
+ *     }                                                                           
+ *     // If odd number, print                                                     
+ *     if((x % 2 != 0) && (x < 99)){                                               
+ *         cout << x << "\n";                                                      
+ *         printOdds(x + 2);                                                       
+ *     }                                                                           
+ *     // If even number, move on                                                  
+ *     if((x % 2 == 0) && (x < 99)){                                               
+ *         printOdds(x + 1);                                                       
+ *     }                                                                           
+ * }                                                                               
+ * 
+ */
+
---
+++ d/sumOfDigits/sumOfDigits.d
@@ -0,0 +1,96 @@
+﻿module main;
+
+import std.stdio;	//input / output
+import std.conv;	//string to int
+import std.file;	//file operations
+import std.string;	//splitLines
+
+void main(string[] args) {
+	string fileName = args[1];
+	if(exists(fileName)){
+		foreach(line; readText(fileName).splitLines()){
+			int x = stringToInt(line);
+			int sum = sumOfDigits(x);
+			writeln(sum);
+		}
+	}
+	else{
+		writeln("Can't find the file");
+	}
+}
+
+int stringToInt(string s){
+	// Converting string to int
+	string clean = strip(s);
+	int i = to!int(clean);
+	return i;
+}
+
+int sumOfDigits(int i){
+	// Catch negatives
+	if(i < 0){
+		return -sumOfDigits(-i);
+	}
+	if(i < 10){
+		return i;
+	}
+	return(i % 10 + sumOfDigits(i / 10));
+}
+
+/**
+ * Original C++ Code
+ * 
+ * #include <iostream>
+ * using std::cout;
+ * #include <string>
+ * using std::string;
+ * #include <fstream>
+ * using std::ifstream;
+ * #include <vector>
+ * using std::vector;
+ * #include <algorithm>
+ * using std::copy;
+ * #include <iterator>
+ * using std::istream_iterator;
+ * #include <sstream>
+ * using std::stringstream;
+ *
+ * int strToInt(string &s);
+ * int sumOfDigits(const int &x);
+ * 
+ * int main(int argc, const char * argv[]) {
+ *     
+ *     ifstream in(argv[1]);
+ *     vector<string> lines;
+ *     
+ *     copy(istream_iterator<string>(in),
+ *          istream_iterator<string>(),
+ *          back_inserter(lines));
+ *     
+ *     in.close();
+ *     
+ *     for(int i = 0; i < lines.size(); ++i){
+ *         cout << sumOfDigits(strToInt(lines[i])) << "\n";
+ *     }
+ *     
+ *     return 0;
+ * }
+ * 
+ * int strToInt(string &s){
+ *     stringstream vert;
+ *     int i;
+ *     vert << s;
+ *     vert >> i;
+ *     return i;
+ * }
+ * 
+ * // Pass by reference to avoid time, memory penalities (small as they would be)
+ * int sumOfDigits(const int &x){
+ *     // Catch negatives?
+ *    if(x < 0)
+ *         return -sumOfDigits(-x);
+ *     if(x < 10)
+ *         return x;
+ *     return(x % 10 + sumOfDigits(x / 10));
+ * }
+ */
